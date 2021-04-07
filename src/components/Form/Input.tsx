d69a7b18e0d2cfe92957089d067286baca5c2ea4
igor-cotrim <igorxuxicotrim@gmail.com>
2021-04-07 12:31:13 -0300

    validation with hookform and yup

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,19 +1,31 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({name, label, ...rest}: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
       <ChakraInput
         id={name}
         name={name}
-        type="email"
+        type={name}
         focusBorderColor="pink.500"
         bgColor="gray.900"
         variant="filled"
@@ -21,8 +33,13 @@ export function Input({name, label, ...rest}: InputProps) {
         _hover={{
           bgColor: "gray.900",
         }}
+        ref={ref}
         {...rest}
       />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
